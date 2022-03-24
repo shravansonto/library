805e314e74858e4d95385e257aa0956db5654d1f
shravan <shravan4uppin@gmail.com>
2022-03-24 18:22:28 +0530

    New updates for heroku deployment

---
+++ apis/tests.py
@@ -0,0 +1,25 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from books.models import Book
+
+
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.book = Book.objects.create(
+            title="Django for APIs",
+            subtitle="Build web APIs with Python and Django",
+            author="william S. Vincent",
+            isbn="9876543210987",
+        )
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("book_list"))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(Book.objects.count(), 1)
+
+        self.assertContains(response, self.book)
